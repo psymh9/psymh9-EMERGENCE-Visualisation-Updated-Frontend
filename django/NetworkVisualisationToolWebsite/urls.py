71e0dd0521eb4784bbe26e6ef98ecada4ddd0bb9
Maximus Hughes-Gray <psymh9@nottingham.ac.uk>
2024-02-21 12:22:26 +0000

    Add How-to and About Pages

--- django/NetworkVisualisationToolWebsite/urls.py
+++ django/NetworkVisualisationToolWebsite/urls.py
@@ -4,4 +4,6 @@ from NetworkVisualisationToolWebsite import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("about/", views.about, name="about"),
+    path("how-to/", views.howto, name="how-to"),
 ]
